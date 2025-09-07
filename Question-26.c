00f9acaf020ab0a0a561302e80d87354e09a5160
Sneha <snehajaisewal14@gmail.com>
2025-09-07 10:59:44 +0530

    DAY-13

---
+++ Question-26.c
@@ -0,0 +1,15 @@
+//Q26: Write a program to print numbers from 1 to n.
+
+#include <stdio.h>
+
+int main(){
+	int input = 0, num =1;
+	scanf("%d", &input);
+	while ( num <= input ){
+		printf("%d ", num);
+		num ++;
+		
+		}
+	return 0 ;
+	}
+		
