bec29238f635e81fa750cf474e1dccffd246e662
Sneha <snehajaisewal14@gmail.com>
2025-10-13 09:36:30 +0530

    DAY-43

---
+++ Question-85.c
@@ -0,0 +1,15 @@
+//Q85: Reverse a string.
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[1000];
+    fgets(str, sizeof(str), stdin);
+    int len = 0;
+    while (str[len] != '\0' && str[len] != '\n') len++;
+    for (int i = len - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+    return 0;
+}
