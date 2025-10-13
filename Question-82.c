857a413421aea063ad638eaf46224ee721c57e00
Sneha <snehajaisewal14@gmail.com>
2025-10-13 09:33:39 +0530

    DAY-41

---
+++ Question-82.c
@@ -0,0 +1,12 @@
+//Q82: Print each character of a string on a new line.
+
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    fgets(str, sizeof(str), stdin);
+    for (int i = 0; str[i] != '\0' && str[i] != '\n'; i++) {
+        printf("%c\n", str[i]);
+    }
+    return 0;
+}
