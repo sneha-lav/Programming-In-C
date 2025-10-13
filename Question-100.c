2934cb24585bead8707ef79a30f1164203a4032d
Sneha <snehajaisewal14@gmail.com>
2025-10-13 10:00:03 +0530

    DAY-50

---
+++ Question-100.c
@@ -0,0 +1,22 @@
+//Q100: Print all sub-strings of a string.
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[1000];
+    fgets(str, sizeof(str), stdin);
+    int len = 0;
+    while (str[len] != '\0' && str[len] != '\n') len++;
+
+    for (int i = 0; i < len; i++) {
+        for (int j = i; j < len; j++) {
+            for (int k = i; k <= j; k++) {
+                printf("%c", str[k]);
+            }
+            if (!(i == len-1 && j == len-1)) printf(",");
+        }
+    }
+    printf("\n");
+    return 0;
+}
